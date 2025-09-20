5ec277616979b3f8984089933989d43137fce2e2
ashwani-199 <ashwani844959@gmail.com>
2025-09-20 05:23:21 -0500

    20Sep2025

--- tution_management/settings.py
+++ tution_management/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-STATIC_ROOT = "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# STATIC_ROOT = "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
