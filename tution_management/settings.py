5ac8583e6778d9fd2a389c62b8a13897a6ffcec9
ashwani-199 <ashwani844959@gmail.com>
2026-04-08 18:05:07 +0500

    08April2026

--- tution_management/settings.py
+++ tution_management/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
